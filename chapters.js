a7f798c00857432b96d0000bc298070fddad1254
PalletHitMe <shreyasranjan00@gmail.com>
2026-02-17 17:37:53 -0500

    Add Chapter 10: Darkness Falls

--- chapters.js
+++ chapters.js
@@ -2,6 +2,8 @@
 // Edit this file only to add/update chapter metadata
 
 const chapters = [
+    { number: 10, title: "Darkness Falls", date: "2026-02-17" },
+
     { number: 11, title: "asdf", date: "2026-02-17" },
     { number: 10, title: "Darkness Falls", date: "2024-02-15" },
     { number: 9, title: "The Prophecy", date: "2024-02-10" },
@@ -13,6 +15,7 @@ const chapters = [
     { number: 3, title: "First Steps", date: "2024-01-10" },
     { number: 2, title: "Awakening", date: "2024-01-05" },
     { number: 1, title: "The Beginning", date: "2024-01-01" }
+
 ];
 
 // Helper functions - don't edit these
