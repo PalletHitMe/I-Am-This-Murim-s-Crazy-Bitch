b24613ced75c1163eaf38831fc7ef0ac4771316d
PalletHitMe <shreyasranjan00@gmail.com>
2026-02-17 18:15:19 -0500

    Change GitHub API token in config.js
    Updated GitHub API token in configuration.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // This file is ignored by git (in .gitignore) so your token stays secret
 
 const GITHUB_CONFIG = {
-    token: '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', // Paste your token between the quotes
+    token: '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', // Paste your token between the quotes
     owner: 'PalletHitMe',
     repo: 'I-Am-This-Murim-s-Crazy-Bitch',
     branch: 'main'
